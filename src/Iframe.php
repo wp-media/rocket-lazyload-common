8d036325cd26904abb7f879e54a19434349d76b6
Rémy Perona <remperona@gmail.com>
2019-01-09 12:30:37 -0500

    capitalize filename

---
+++ src/Iframe.php
@@ -0,0 +1,170 @@
+<?php
+namespace RocketLazyload;
+
+/**
+ * A class to provide the methods needed to lazyload iframes in WP Rocket and Lazyload by WP Rocket
+ */
+class Iframe
+{
+    /**
+     * Finds iframes in the HTML provided and call the methods to lazyload them
+     *
+     * @param string $html HTML to parse
+     * @param array $args Array of arguments to use
+     * @return string
+     */
+    public function lazyloadIframes($html, $args)
+    {
+        $defaults = [
+            'youtube' => false,
+        ];
+
+        $args = wp_parse_args($args, $defaults);
+
+        preg_match_all('@<iframe(?<atts>\s.+)>.*</iframe>@iUs', $html, $matches, PREG_SET_ORDER);
+
+        if (empty($matches)) {
+            return $html;
+        }
+
+        foreach ($matches as $iframe) {
+            if ($this->isIframeExcluded($iframe)) {
+                continue;
+            }
+
+            // Given the previous regex pattern, $iframe['atts'] starts with a whitespace character.
+            if (! preg_match('@\ssrc\s*=\s*(\'|")(?<src>.*)\1@iUs', $iframe['atts'], $atts)) {
+                continue;
+            }
+
+            $iframe['src'] = trim($atts['src']);
+
+            if ('' === $iframe['src']) {
+                continue;
+            }
+
+            if ($args['youtube']) {
+                $iframe_lazyload = $this->replaceYoutubeThumbnail($iframe);
+            }
+
+            if (empty($iframe_lazyload)) {
+                $iframe_lazyload = $this->replaceIframe($iframe);
+            }
+
+            $html = str_replace($iframe[0], $iframe_lazyload, $html);
+        }
+
+        return $html;
+    }
+
+    /**
+     * Checks if the provided iframe is excluded from lazyload
+     *
+     * @param array $iframe Array of matched patterns
+     * @return boolean
+     */
+    private function isIframeExcluded($iframe)
+    {
+        // Don't mess with the Gravity Forms ajax iframe.
+        if (strpos($iframe[0], 'gform_ajax_frame')) {
+            return true;
+        }
+
+        // Don't lazyload if iframe has data-no-lazy attribute.
+        if (strpos($iframe[0], 'data-no-lazy=')) {
+            return true;
+        }
+
+        // Don't lazyload if iframe is google recaptcha fallback.
+        if (strpos($iframe[0], 'recaptcha/api/fallback')) {
+            return true;
+        }
+
+        return false;
+    }
+
+    /**
+     * Applies lazyload on the iframe provided
+     *
+     * @param array $iframe Array of matched elements
+     * @return string
+     */
+    private function replaceIframe($iframe)
+    {
+        /**
+         * Filter the LazyLoad placeholder on src attribute
+         *
+         * @since 2.11
+         *
+         * @param string $placeholder placeholder that will be printed.
+         */
+        $placeholder = apply_filters('rocket_lazyload_placeholder', 'about:blank');
+
+        $placeholder_atts = str_replace($iframe['src'], $placeholder, $iframe['atts']);
+        $iframe_lazyload  = str_replace($iframe['atts'], $placeholder_atts . ' data-rocket-lazyload="fitvidscompatible" data-lazy-src="' . esc_url($iframe['src']) . '"', $iframe[0]);
+
+        /**
+         * Filter the LazyLoad HTML output on iframes
+         *
+         * @since 2.11
+         *
+         * @param array $html Output that will be printed.
+         */
+        $iframe_lazyload  = apply_filters('rocket_lazyload_iframe_html', $iframe_lazyload);
+        $iframe_lazyload .= '<noscript>' . $iframe[0] . '</noscript>';
+
+        return $iframe_lazyload;
+    }
+
+    /**
+     * Replaces the iframe provided by the Youtube thumbnail
+     *
+     * @param array $iframe Array of matched elements
+     * @return bool|string
+     */
+    private function replaceYoutubeThumbnail($iframe)
+    {
+        $youtube_id = $this->getYoutubeIDFromURL($iframe['src']);
+
+        if (! $youtube_id) {
+            return false;
+        }
+
+        $query = wp_parse_url(htmlspecialchars_decode($iframe['src']), PHP_URL_QUERY);
+
+        /**
+         * Filter the LazyLoad HTML output on Youtube iframes
+         *
+         * @since 2.11
+         *
+         * @param array $html Output that will be printed.
+         */
+        $youtube_lazyload  = apply_filters('rocket_lazyload_youtube_html', '<div class="rll-youtube-player" data-id="' . esc_attr($youtube_id) . '" data-query="' . esc_attr($query) . '"></div>');
+        $youtube_lazyload .= '<noscript>' . $iframe[0] . '</noscript>';
+
+        return $youtube_lazyload;
+    }
+
+    /**
+     * Gets the Youtube ID from the URL provided
+     *
+     * @param string $url URL to search
+     * @return bool|string
+     */
+    private function getYoutubeIDFromURL($url)
+    {
+        $pattern = '#^(?:https?:)?(?://)?(?:www\.)?(?:youtu\.be|youtube\.com|youtube-nocookie\.com)/(?:embed/|v/|watch/?\?v=)([\w-]{11})#iU';
+        $result  = preg_match($pattern, $url, $matches);
+
+        if (! $result) {
+            return false;
+        }
+
+        // exclude playlist.
+        if ('videoseries' === $matches[1]) {
+            return false;
+        }
+
+        return $matches[1];
+    }
+}
---
+++ src/Image.php
@@ -0,0 +1,248 @@
+<?php
+namespace RocketLazyload;
+
+/**
+ * A class to provide the methods needed to lazyload images in WP Rocket and Lazyload by WP Rocket
+ */
+class Image
+{
+    /**
+     * Finds the images to be lazyloaded and call the callback method to replace them.
+     *
+     * @param string $html
+     * @return string
+     */
+    public function lazyloadImages($html)
+    {
+        return preg_replace_callback('#<img([^>]*) src=("(?:[^"]+)"|\'(?:[^\']+)\'|(?:[^ >]+))([^>]*)>#', [$this,'imageReplaceCallback'], $html);
+    }
+
+    /**
+     * Modifies the HTML image tag provided to apply lazyload on it.
+     *
+     * @param array $image Array of matching patterns.
+     * @return string
+     */
+    private function imageReplaceCallback($image)
+    {
+        // Don't apply LazyLoad on images from WP Retina x2.
+        if (function_exists('wr2x_picture_rewrite')) {
+            if (wr2x_get_retina(trailingslashit(ABSPATH) . wr2x_get_pathinfo_from_image_src(trim($image[2], '"')))) {
+                return $image[0];
+            }
+        }
+
+        /**
+         * Filters the attributes used to prevent lazylad from being applied
+         *
+         * @since 2.11
+         * @author Remy Perona
+         *
+         * @param array $excluded_attributes An array of excluded attributes.
+         */
+        $excluded_attributes = apply_filters(
+            'rocket_lazyload_excluded_attributes',
+            [
+                'data-src=',
+                'data-no-lazy=',
+                'data-lazy-original=',
+                'data-lazy-src=',
+                'data-lazysrc=',
+                'data-lazyload=',
+                'data-bgposition=',
+                'data-envira-src=',
+                'fullurl=',
+                'lazy-slider-img=',
+                'data-srcset=',
+                'class="ls-l',
+                'class="ls-bg',
+            ]
+        );
+
+        /**
+         * Filters the src used to prevent lazylad from being applied
+         *
+         * @since 2.11
+         * @author Remy Perona
+         *
+         * @param array $excluded_src An array of excluded src.
+         */
+        $excluded_src = apply_filters(
+            'rocket_lazyload_excluded_src',
+            [
+                '/wpcf7_captcha/',
+                'timthumb.php?src',
+            ]
+        );
+
+        if ($this->isExcluded($image[1] . $image[3], $excluded_attributes) || $this->isExcluded($image[2], $excluded_src)) {
+            return $image[0];
+        }
+
+        $html = sprintf('<img%1$s src="%4$s" data-lazy-src=%2$s%3$s>', $image[1], $image[2], $image[3], $this->getPlaceholder());
+
+        $html_noscript = sprintf('<noscript><img%1$s src=%2$s%3$s></noscript>', $image[1], $image[2], $image[3]);
+
+        /**
+         * Filter the LazyLoad HTML output
+         *
+         * @since 1.0.2
+         *
+         * @param array $html Output that will be printed
+         */
+        $html = apply_filters('rocket_lazyload_html', $html);
+
+        return $html . $html_noscript;
+    }
+
+    /**
+     * Checks if the provided string matches with the provided excluded patterns
+     *
+     * @param string $string String to check
+     * @param array $excluded_values Patterns to match against
+     * @return boolean
+     */
+    private function isExcluded($string, $excluded_values)
+    {
+        foreach ($excluded_values as $excluded_value) {
+            if (strpos($string, $excluded_value) !== false) {
+                return true;
+            }
+        }
+    
+        return false;
+    }
+
+    /**
+     * Applies lazyload on srcset and sizes attributes
+     *
+     * @param string $html HTML image tag
+     * @return string
+     */
+    public function lazyloadResponsiveAttributes($html)
+    {
+        if (preg_match('/srcset=("(?:[^"]+)"|\'(?:[^\']+)\'|(?:[^ >]+))/i', $html)) {
+            $html = str_replace('srcset=', 'data-lazy-srcset=', $html);
+        }
+    
+        if (preg_match('/sizes=("(?:[^"]+)"|\'(?:[^\']+)\'|(?:[^ >]+))/i', $html)) {
+            $html = str_replace('sizes=', 'data-lazy-sizes=', $html);
+        }
+    
+        return $html;
+    }
+
+    /**
+     * Finds patterns matching smiley and call the callback method to replace them with the image
+     *
+     * @param string $text Content to search in
+     * @return string
+     */
+    public function convertSmilies($text)
+    {
+        global $wp_smiliessearch;
+
+        if (! get_option('use_smilies') || empty($wp_smiliessearch)) {
+            return $text;
+        }
+
+        $output = '';
+        // HTML loop taken from texturize function, could possible be consolidated.
+        $textarr = preg_split('/(<.*>)/U', $text, -1, PREG_SPLIT_DELIM_CAPTURE); // capture the tags as well as in between.
+        $stop    = count($textarr);// loop stuff.
+
+        // Ignore proessing of specific tags.
+        $tags_to_ignore       = 'code|pre|style|script|textarea';
+        $ignore_block_element = '';
+
+        for ($i = 0; $i < $stop; $i++) {
+            $content = $textarr[ $i ];
+
+            // If we're in an ignore block, wait until we find its closing tag.
+            if ('' === $ignore_block_element && preg_match('/^<(' . $tags_to_ignore . ')>/', $content, $matches)) {
+                $ignore_block_element = $matches[1];
+            }
+
+            // If it's not a tag and not in ignore block.
+            if ('' === $ignore_block_element && strlen($content) > 0 && '<' !== $content[0]) {
+                $content = preg_replace_callback($wp_smiliessearch, [$this, 'translateSmiley'], $content);
+            }
+
+            // did we exit ignore block.
+            if ('' !== $ignore_block_element && '</' . $ignore_block_element . '>' === $content) {
+                $ignore_block_element = '';
+            }
+
+            $output .= $content;
+        }
+
+        return $output;
+    }
+
+    /**
+     * Replace matches by smiley image, lazyloaded
+     *
+     * @param array $matches Array of matches
+     * @return string
+     */
+    private function translateSmiley($matches)
+    {
+        global $wpsmiliestrans;
+
+        if (count($matches) === 0) {
+            return '';
+        }
+
+        $smiley = trim(reset($matches));
+        $img    = $wpsmiliestrans[ $smiley ];
+
+        $matches    = [];
+        $ext        = preg_match('/\.([^.]+)$/', $img, $matches) ? strtolower($matches[1]) : false;
+        $image_exts = ['jpg', 'jpeg', 'jpe', 'gif', 'png'];
+
+        // Don't convert smilies that aren't images - they're probably emoji.
+        if (! in_array($ext, $image_exts, true)) {
+            return $img;
+        }
+
+        /**
+         * Filter the Smiley image URL before it's used in the image element.
+         *
+         * @since 2.9.0
+         *
+         * @param string $smiley_url URL for the smiley image.
+         * @param string $img        Filename for the smiley image.
+         * @param string $site_url   Site URL, as returned by site_url().
+         */
+        $src_url = apply_filters('smilies_src', includes_url("images/smilies/$img"), $img, site_url());
+
+        // Don't LazyLoad if process is stopped for these reasons.
+        if (is_feed() || is_preview()) {
+            return sprintf(' <img src="%s" alt="%s" class="wp-smiley" /> ', esc_url($src_url), esc_attr($smiley));
+        }
+
+        return sprintf(' <img src="%s" data-lazy-src="%s" alt="%s" class="wp-smiley" /> ', $this->getPlaceholder(), esc_url($src_url), esc_attr($smiley));
+    }
+
+    /**
+     * Returns the placeholder for the src attribute
+     *
+     * @since 1.2
+     * @author Remy Perona
+     *
+     * @param int $width  Width of the placeholder image. Default 1.
+     * @param int $height Height of the placeholder image. Default 1.
+     * @return void
+     */
+    private function getPlaceholder($width = 1, $height = 1)
+    {
+        /**
+         * Filter the image lazyLoad placeholder on src attribute
+         *
+         * @since 1.1
+         *
+         * @param string $placeholder Placeholder that will be printed.
+         */
+        return apply_filters('rocket_lazyload_placeholder', "data:image/svg+xml,%3Csvg xmlns=\'http://www.w3.org/2000/svg\' viewBox=\'0 0 $width $height\'%3E%3C/svg%3E");
+    }
+}
