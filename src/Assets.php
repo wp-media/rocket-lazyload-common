b3ca84cc6cf4abbfc4c0b2663447d15a8817f1dc
Rémy Perona <remperona@gmail.com>
2019-06-04 10:10:29 -0400

    improve infinite scroll support

--- src/Assets.php
+++ src/Assets.php
@@ -118,16 +118,28 @@ class Assets
                             }
 
                             imgs = mutation.addedNodes[i].getElementsByTagName(\'img\');
+                            is_image = mutation.addedNodes[i].tagName == "IMG";
                             iframes = mutation.addedNodes[i].getElementsByTagName(\'iframe\');
+                            is_iframe = mutation.addedNodes[i].tagName == "IFRAME";
                             rocket_lazy = mutation.addedNodes[i].getElementsByClassName(\'rocket-lazyload\');
 
-                            if ( 0 === imgs.length && 0 === iframes.length && 0 === rocket_lazy.length ) {
-                                return;
+                            image_count += images.length;
+			                iframe_count += iframes.length;
+			                rocketlazy_count += rocket_lazy.length;
+                            
+                            if(is_image){
+                                image_count += 1;
                             }
 
-                            lazyLoadInstance.update();
+                            if(is_iframe){
+                                iframe_count += 1;
+                            }
                         }
                     } );
+
+                    if(image_count > 0 || iframe_count > 0 || rocketlazy_count > 0){
+                        lazyLoadInstance.update();
+                    }
                 } );
                 
                 var b      = document.getElementsByTagName("body")[0];
