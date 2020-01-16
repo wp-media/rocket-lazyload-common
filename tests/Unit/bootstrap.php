e4f0a4a143056b05b74652bee5da3dc9f15bab51
Rémy Perona <remperona@gmail.com>
2020-01-16 18:26:40 -0500

    update unit tests bootstrap

--- tests/Unit/bootstrap.php
+++ tests/Unit/bootstrap.php
@@ -5,18 +5,9 @@
  * @package RocketLazyload\Tests\Unit
  */
 
-if (version_compare(phpversion(), '5.6.0', '<')) {
-    die('Rocket Lazyload Common Unit Tests require PHP 5.6 or higher.');
-}
+namespace RocketLazyload\Tests\Unit;
 
-define('RLL_COMMON_TESTS_ROOT', __DIR__);
-define('RLL_COMMON_ROOT', dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR);
+use function RocketLazyload\Tests\init_test_suite;
 
-$rll_common_autoload_path = RLL_COMMON_ROOT . 'vendor/';
-
-if (! file_exists($rll_common_autoload_path . 'autoload.php')) {
-    die('Whoops, we need Composer before we start running tests.  Please type: `composer install`.  When done, try running `phpunit` again.');
-}
-
-require_once $rll_common_autoload_path . 'autoload.php';
-unset($rll_common_autoload_path);
+require_once dirname( dirname( __FILE__ ) ) . '/bootstrap-functions.php';
+init_test_suite( 'Unit' );
