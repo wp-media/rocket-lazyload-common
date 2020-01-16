8ed9dd8329801da4bbcbab41b101922bb2b50883
Rémy Perona <remperona@gmail.com>
2020-01-16 18:29:37 -0500

    update integration bootstrap file

--- tests/Integration/bootstrap.php
+++ tests/Integration/bootstrap.php
@@ -5,41 +5,51 @@
  * @package Rocketlazyload\Tests\Integration
  */
 
-if (version_compare(phpversion(), '5.6.0', '<')) {
-    die('Rocket Lazyload Common Integration Tests require PHP 5.6 or higher.');
-}
+namespace Rocketlazyload\Tests\Integration;
 
-// Define testing constants.
-define('RLL_COMMON_TESTS_ROOT', __DIR__);
-define('RLL_COMMON_ROOT', dirname(dirname(__DIR__)));
+use function Rocketlazyload\Tests\init_test_suite;
+
+require_once dirname( dirname( __FILE__ ) ) . '/boostrap-functions.php';
+init_test_suite( 'Integration' );
 
 /**
- * Gets the WP tests suite directory
+ * Get the WordPress' tests suite directory.
  *
- * @return string
+ * @return string Returns The directory path to the WordPress testing environment.
  */
-function rocketlazyloadGetWPTestsDir()
-{
-    $tests_dir = getenv('WP_TESTS_DIR');
+function get_wp_tests_dir() {
+    $tests_dir = getenv( 'WP_TESTS_DIR' );
 
     // Travis CI & Vagrant SSH tests directory.
-    if (empty($tests_dir)) {
+    if ( empty( $tests_dir ) ) {
         $tests_dir = '/tmp/wordpress-tests-lib';
     }
+
     // If the tests' includes directory does not exist, try a relative path to Core tests directory.
-    if (! file_exists($tests_dir . '/includes/')) {
+    if ( ! file_exists( $tests_dir . '/includes/' ) ) {
         $tests_dir = '../../../../tests/phpunit';
     }
+
     // Check it again. If it doesn't exist, stop here and post a message as to why we stopped.
-    if (! file_exists($tests_dir . '/includes/')) {
-        trigger_error('Unable to run the integration tests, as the WordPress test suite could not be located.', E_USER_ERROR);  // @codingStandardsIgnoreLine.
+    if ( ! file_exists( $tests_dir . '/includes/' ) ) {
+        trigger_error( 'Unable to run the integration tests, because the WordPress test suite could not be located.', E_USER_ERROR ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_trigger_error -- Valid use case for our testing suite.
     }
+
     // Strip off the trailing directory separator, if it exists.
-    return rtrim($tests_dir, DIRECTORY_SEPARATOR);
+    return rtrim( $tests_dir, DIRECTORY_SEPARATOR );
 }
 
-$rocket_ll_tests_dir = rocketlazyloadGetWPTestsDir();
+/**
+ * Bootstraps the integration testing environment with WordPress and WP Rocket.
+ *
+ * @param string $wp_tests_dir The directory path to the WordPress testing environment.
+ */
+function bootstrap_integration_suite( $wp_tests_dir ) {
+    // Give access to tests_add_filter() function.
+    require_once $wp_tests_dir . '/includes/functions.php';
 
-require_once $rocket_ll_tests_dir . '/includes/bootstrap.php';
+    // Start up the WP testing environment.
+    require_once $wp_tests_dir . '/includes/bootstrap.php';
+}
 
-unset($rocket_ll_tests_dir);
+bootstrap_integration_suite( get_wp_tests_dir() );
