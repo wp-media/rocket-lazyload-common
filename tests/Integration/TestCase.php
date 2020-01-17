6f7fb46716b3512fef1f2beefcbe5803112e4b49
Rémy Perona <remperona@gmail.com>
2020-01-17 11:49:28 -0500

    add integration testCase

---
+++ tests/Integration/TestCase.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Test Case for all of the integration tests.
+ *
+ * @package RocketLazyload\Tests\Integration
+ */
+
+namespace RocketLazyload\Tests\Integration;
+
+use Brain\Monkey;
+use WP_UnitTestCase;
+
+/**
+ * Test Case for all of the integration tests.
+ */
+abstract class TestCase extends WP_UnitTestCase
+{
+    /**
+     * Prepares the test environment before each test.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        Monkey\setUp();
+    }
+
+    /**
+     * Cleans up the test environment after each test.
+     */
+    public function tearDown()
+    {
+        Monkey\tearDown();
+        parent::tearDown();
+    }
+}
