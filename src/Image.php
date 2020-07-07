0d81d6191b4dac394338d09caf830f5fef8fcf72
Ahmed Saeed <eng.ahmeds3ed@gmail.com>
2020-07-07 17:24:28 +0200

    strip html tags and add more tests

--- src/Image.php
+++ src/Image.php
@@ -66,7 +66,15 @@ class Image {
 				continue;
 			}
 
-			$url['url'] = esc_url( trim( html_entity_decode($url['url'], ENT_QUOTES|ENT_HTML5), '\'" ' ) );
+			$url['url'] = esc_url(
+				trim(
+					strip_tags(
+						html_entity_decode(
+							$url['url'], ENT_QUOTES|ENT_HTML5
+						)
+					), '\'" '
+				)
+			);
 
 			if ( $this->isExcluded( $url['url'], $this->getExcludedSrc() ) ) {
 				continue;
