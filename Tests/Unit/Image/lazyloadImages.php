7a44bf97463ae2ffbf7dd7fc614baceca85d5d91
Rémy Perona <remperona@gmail.com>
2021-09-30 14:27:38 -0400

    update tests and fixtures

--- Tests/Unit/Image/lazyloadImages.php
+++ Tests/Unit/Image/lazyloadImages.php
@@ -34,13 +34,23 @@ class Test_LazyloadImages extends TestCase {
 		);
 	}
 
-	public function testShouldReturnImagesLazyloaded() {
+	public function testShouldReturnImagesLazyloadedNoNative() {
 		Functions\when( 'absint' )->alias( function( $value ) {
 			return abs( intval( $value ) );
 		} );
 
 		$original = file_get_contents( RLL_COMMON_ROOT . 'Tests/Fixtures/image/images.html' );
 		$expected = file_get_contents( RLL_COMMON_ROOT . 'Tests/Fixtures/image/imageslazyloaded.html' );
+
+		$this->assertSame(
+			$expected,
+			$this->image->lazyloadImages( $original, $original, false )
+		);
+	}
+
+	public function testShouldReturnImagesLazyloadedNative() {
+		$original = file_get_contents( RLL_COMMON_ROOT . 'Tests/Fixtures/image/images.html' );
+		$expected = file_get_contents( RLL_COMMON_ROOT . 'Tests/Fixtures/image/imageslazyloadednative.html' );
 
 		$this->assertSame(
 			$expected,
