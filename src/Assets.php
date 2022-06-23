d08c059f15c64d1c812031044c881a6e92562d99
Michael Lee <michaelleemichaellee408@gmail.com>
2022-06-23 12:27:02 +0100

    Added new instance of lazyload for background image

--- src/Assets.php
+++ src/Assets.php
@@ -69,8 +69,14 @@ class Assets {
 		$script = '';
 
 		$args['options'] = array_intersect_key( $args['options'], $allowed_options );
+		$script         .= 'window.lazyLoadOptions = ';
 
-		$script .= 'window.lazyLoadOptions = {
+		if ( isset( $args['elements']['background_image'] ) ) {
+			$script .= '[';
+		}
+
+		$script .= '
+			{
                 elements_selector: "' . esc_attr( implode( ',', $args['elements'] ) ) . '",
                 data_src: "lazy-src",
                 data_srcset: "lazy-srcset",
@@ -100,56 +106,58 @@ class Assets {
 			$script = rtrim( $script, ',' );
 		}
 
-		$script .= '};';
+		if ( isset( $args['elements']['background_image'] ) ) {
+			$script .= '},{
+				elements_selector: "' . esc_attr( $args['elements']['background_image'] ) . '",
+				data_src: "lazy-src",
+				data_srcset: "lazy-srcset",
+				data_sizes: "lazy-sizes",
+				class_loading: "lazyloading",
+				class_loaded: "lazyloaded",
+				threshold: ' . esc_attr( $args['threshold'] ) . ',
+			}];';
+		} else {
+			$script .= '};';
+		}
 
 		$script .= '
         window.addEventListener(\'LazyLoad::Initialized\', function (e) {
             var lazyLoadInstance = e.detail.instance;
-
             if (window.MutationObserver) {
                 var observer = new MutationObserver(function(mutations) {
                     var image_count = 0;
                     var iframe_count = 0;
                     var rocketlazy_count = 0;
-
                     mutations.forEach(function(mutation) {
                         for (var i = 0; i < mutation.addedNodes.length; i++) {
                             if (typeof mutation.addedNodes[i].getElementsByTagName !== \'function\') {
                                 continue;
                             }
-
                            if (typeof mutation.addedNodes[i].getElementsByClassName !== \'function\') {
                                 continue;
                             }
-
                             images = mutation.addedNodes[i].getElementsByTagName(\'img\');
                             is_image = mutation.addedNodes[i].tagName == "IMG";
                             iframes = mutation.addedNodes[i].getElementsByTagName(\'iframe\');
                             is_iframe = mutation.addedNodes[i].tagName == "IFRAME";
                             rocket_lazy = mutation.addedNodes[i].getElementsByClassName(\'rocket-lazyload\');
-
                             image_count += images.length;
 			                iframe_count += iframes.length;
 			                rocketlazy_count += rocket_lazy.length;
-
                             if(is_image){
                                 image_count += 1;
                             }
-
                             if(is_iframe){
                                 iframe_count += 1;
                             }
                         }
                     } );
-
                     if(image_count > 0 || iframe_count > 0 || rocketlazy_count > 0){
                         lazyLoadInstance.update();
                     }
                 } );
-
                 var b      = document.getElementsByTagName("body")[0];
                 var config = { childList: true, subtree: true };
-
                 observer.observe(b, config);
             }
         }, false);';
