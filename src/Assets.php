b70cb8a3b4cb739befc7634b3adfd845699c2ae6
Michael Lee <michaelleemichaellee408@gmail.com>
2022-06-24 10:36:29 +0100

    Fix test

--- src/Assets.php
+++ src/Assets.php
@@ -75,8 +75,7 @@ class Assets {
 			$script .= '[';
 		}
 
-		$script .= '
-			{
+		$script .= '{
                 elements_selector: "' . esc_attr( implode( ',', $args['elements'] ) ) . '",
                 data_src: "lazy-src",
                 data_srcset: "lazy-srcset",
@@ -123,41 +122,51 @@ class Assets {
 		$script .= '
         window.addEventListener(\'LazyLoad::Initialized\', function (e) {
             var lazyLoadInstance = e.detail.instance;
+
             if (window.MutationObserver) {
                 var observer = new MutationObserver(function(mutations) {
                     var image_count = 0;
                     var iframe_count = 0;
                     var rocketlazy_count = 0;
+
                     mutations.forEach(function(mutation) {
                         for (var i = 0; i < mutation.addedNodes.length; i++) {
                             if (typeof mutation.addedNodes[i].getElementsByTagName !== \'function\') {
                                 continue;
                             }
-                           if (typeof mutation.addedNodes[i].getElementsByClassName !== \'function\') {
+
+                            if (typeof mutation.addedNodes[i].getElementsByClassName !== \'function\') {
                                 continue;
                             }
+
                             images = mutation.addedNodes[i].getElementsByTagName(\'img\');
                             is_image = mutation.addedNodes[i].tagName == "IMG";
                             iframes = mutation.addedNodes[i].getElementsByTagName(\'iframe\');
                             is_iframe = mutation.addedNodes[i].tagName == "IFRAME";
                             rocket_lazy = mutation.addedNodes[i].getElementsByClassName(\'rocket-lazyload\');
+
                             image_count += images.length;
 			                iframe_count += iframes.length;
 			                rocketlazy_count += rocket_lazy.length;
+
                             if(is_image){
                                 image_count += 1;
                             }
+
                             if(is_iframe){
                                 iframe_count += 1;
                             }
                         }
                     } );
+
                     if(image_count > 0 || iframe_count > 0 || rocketlazy_count > 0){
                         lazyLoadInstance.update();
                     }
                 } );
+
                 var b      = document.getElementsByTagName("body")[0];
                 var config = { childList: true, subtree: true };
+
                 observer.observe(b, config);
             }
         }, false);';
