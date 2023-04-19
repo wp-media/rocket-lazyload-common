96fc1596e1d8171c439ad4288d2066af82455b96
COQUARD Cyrille <coquardcyr@gmail.com>
2023-04-19 14:48:55 +0200

    Fixed CR

--- src/Assets.php
+++ src/Assets.php
@@ -251,13 +251,12 @@ class Assets {
 
 		$args = wp_parse_args( $args, $defaults );
 
-		$base_url = 'https://i.ytimg.com/' . ( 'webp' === $args['extension'] ? 'vi_webp' : 'vi' ) . '/ID/';
+		$image_url = 'https://i.ytimg.com/' . ( 'webp' === $args['extension'] ? 'vi_webp' : 'vi' ) . '/ID/' . $args['resolution'] . '.' . $args['extension'];
 
-		$image = '<img src="' . $base_url . $args['resolution'] . '.' . $args['extension'] . '" alt="" width="' . $allowed_resolutions[ $args['resolution'] ]['width'] . '" height="' . $allowed_resolutions[ $args['resolution'] ]['height'] . '">';
+		$image = '<img src="' . $image_url . '" alt="" width="' . $allowed_resolutions[ $args['resolution'] ]['width'] . '" height="' . $allowed_resolutions[ $args['resolution'] ]['height'] . '">';
 
 		if ( isset( $args['lazy_image'] ) && $args['lazy_image'] ) {
 			$attributes = 'alt="" width="' . $allowed_resolutions[ $args['resolution'] ]['width'] . '" height="' . $allowed_resolutions[ $args['resolution'] ]['height'] . '"';
-			$image_url  = $base_url . $args['resolution'] . '.' . $args['extension'];
 
 			$image = '<img data-lazy-src="' . $image_url . '" ' . $attributes . '><noscript><img src="' . $image_url . '" ' . $attributes . '></noscript>';
 
