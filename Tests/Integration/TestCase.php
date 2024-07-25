5061b1500803a62fcc73edff1762a9c42b5fddbc
Rémy Perona <remperona@gmail.com>
2024-07-25 16:21:25 -0400

    update testCase

--- Tests/Integration/TestCase.php
+++ Tests/Integration/TestCase.php
@@ -1,15 +1,40 @@
 <?php
+declare(strict_types=1);
 
 namespace RocketLazyload\Tests\Integration;
 
+use ReflectionObject;
 use WPMedia\PHPUnit\Integration\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
+	protected $config;
+
 	public function set_up() {
-		parent::set_up();
-	}
+        parent::set_up();
+
+		if ( empty( $this->config ) ) {
+			$this->loadTestDataConfig();
+		}
+    }
 
 	public function tear_down() {
-		parent::tear_down();
+        parent::tear_down();
+    }
+
+	public function configTestData() {
+		if ( empty( $this->config ) ) {
+			$this->loadTestDataConfig();
+		}
+
+		return isset( $this->config['test_data'] )
+			? $this->config['test_data']
+			: $this->config;
+	}
+
+	protected function loadTestDataConfig() {
+		$obj      = new ReflectionObject( $this );
+		$filename = $obj->getFileName();
+
+		$this->config = $this->getTestData( dirname( $filename ), basename( $filename, '.php' ) );
 	}
 }
--- Tests/Unit/TestCase.php
+++ Tests/Unit/TestCase.php
@@ -2,14 +2,38 @@
 
 namespace RocketLazyload\Tests\Unit;
 
+use ReflectionObject;
 use WPMedia\PHPUnit\Unit\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase {
+	protected $config;
+
 	protected function set_up() {
         parent::set_up();
+
+		if ( empty( $this->config ) ) {
+			$this->loadTestDataConfig();
+		}
     }
 
 	protected function tear_down() {
         parent::tear_down();
     }
+
+	public function configTestData() {
+		if ( empty( $this->config ) ) {
+			$this->loadTestDataConfig();
+		}
+
+		return isset( $this->config['test_data'] )
+			? $this->config['test_data']
+			: $this->config;
+	}
+
+	protected function loadTestDataConfig() {
+		$obj      = new ReflectionObject( $this );
+		$filename = $obj->getFileName();
+
+		$this->config = $this->getTestData( dirname( $filename ), basename( $filename, '.php' ) );
+	}
 }
